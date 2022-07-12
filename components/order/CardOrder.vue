232bcc8f4312f5ae1a70e7e8634741fb3f7ae7ad
Arian Derida <arianderida@gmail.com>
2022-07-12 14:05:56 +0700

    feat: add button shoping icon 🔨

--- components/order/CardOrder.vue
+++ components/order/CardOrder.vue
@@ -11,7 +11,7 @@
         <p class="mt-2">{{ item.description | truncate() }}.</p>
         <div class="flex-between">
           <h3>{{ item.unit_price | money() }}</h3>
-          <v-btn icon> <v-icon>mdi-star</v-icon></v-btn>
+          <v-btn icon> <v-icon>mdi-shopping</v-icon></v-btn>
         </div>
       </v-col>
     </v-row>
